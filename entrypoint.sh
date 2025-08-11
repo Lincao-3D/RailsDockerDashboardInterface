5fb11bdcc95df56bbd2b3ecda3ee359286071039
Lincao-3D <lincoln.3d.br@gmail.com>
2025-08-11 18:27:19 -0300

    entry no extra space in bin bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
-# !/bin/bash
+#!/bin/bash
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /home/appuser/app/tmp/pids/server.pid # <-- Use full path or ensure WORKDIR is set before
+# Assuming WORKDIR is /home/appuser/app as set in Dockerfile final stage
+rm -f tmp/pids/server.pid
 
-# RAILS_ENV will be set by Render, so this line is mostly for local consistency if you use this script.
-echo "RAILS_ENV is currently: ${RAILS_ENV}"
-echo "Executing main command: $@"
+# RAILS_ENV will be set by Render (or other production ENV VARS)
+echo "Entrypoint: RAILS_ENV is currently: ${RAILS_ENV}"
+echo "Entrypoint: Executing main command: $@"
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
